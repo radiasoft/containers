36292aba1359fa25e367e8110c2b0da5e1143d6f
robnagler <github@q33.us>
2015-06-15 23:49:30 +0000

    added --with_feenableexcept so halts on first NaN exception

--- container-conf/install-warp.sh
+++ container-conf/install-warp.sh
@@ -26,14 +26,16 @@ cd ..
 
 gcl warp
 cd warp/pywarp90
+FARGS = --with_feenableexcept
+warpg
 make -f Makefile.Forthon \
-    DEBUG='-g --fargs -O0' \
+    DEBUG='-g --fargs "--with_feenableexcept -O0"' \
     SETUP_PY_DEBUG='-g' \
     clean install
 make -f Makefile.Forthon.pympi \
     MPIPARALLEL= \
     FCOMP='-F gfortran --fcompexec /usr/lib64/openmpi/bin/mpifort' \
-    DEBUG='-g --fargs -O0' \
+    DEBUG='-g --fargs "--with_feenableexcept -O0"' \
     SETUP_PY_DEBUG='-g' \
     clean install
 cd ../..
