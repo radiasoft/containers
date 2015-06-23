5755ca0e38ab700c6994c18d6b9db421d3816c32
robnagler <github@q33.us>
2015-06-23 20:12:53 +0000

    warp --with_feenableexcept wasn't working; compile $BUILD_WARP_DEBUG only if set

--- beamsim-conf/install-warp.sh
+++ beamsim-conf/install-warp.sh
@@ -26,14 +26,25 @@ cd ..
 
 gcl warp
 cd warp/pywarp90
-make -f Makefile.Forthon \
-    DEBUG='-g --fargs "--with_feenableexcept -O0"' \
-    SETUP_PY_DEBUG='-g' \
-    clean install
-make -f Makefile.Forthon.pympi \
-    MPIPARALLEL= \
-    FCOMP='-F gfortran --fcompexec /usr/lib64/openmpi/bin/mpifort' \
-    DEBUG='-g --fargs "--with_feenableexcept -O0"' \
-    SETUP_PY_DEBUG='-g' \
-    clean install
+
+if [[ $BUILD_WARP_DEBUG ]]; then
+    make -f Makefile.Forthon \
+        DEBUG='-g --fargs -O0' \
+        FARGS=--with_feenableexcept \
+        SETUP_PY_DEBUG='-g' \
+        clean install
+    make -f Makefile.Forthon.pympi \
+        DEBUG='-g --fargs -O0' \
+        FARGS=--with_feenableexcept \
+        FCOMP='-F gfortran --fcompexec /usr/lib64/openmpi/bin/mpifort' \
+        MPIPARALLEL= \
+        SETUP_PY_DEBUG='-g' \
+        clean install
+else
+    make -f Makefile.Forthon clean install
+    make -f Makefile.Forthon.pympi \
+        FCOMP='-F gfortran --fcompexec /usr/lib64/openmpi/bin/mpifort' \
+        clean install
+fi
+
 cd ../..
