7710e0fc16499e04f1ccb7e56296edc31223687e
robnagler <github@q33.us>
2015-09-25 02:21:56 +0000

    openmpi not in path before openmpi-devel is installed

--- radiasoft/beamsim/codes/common.sh
+++ radiasoft/beamsim/codes/common.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Some rpms most codes use
 codes_yum install atlas-devel blas-devel lapack-devel openmpi-devel
-# Just in case this is installed outside the context for radiasoft/python2
+# Just in case this is installed outside the context for radiasoft/python2,
+# we need openmpi in our path (normally set by ~/.bashrc)
+if [[ ! ( :$PATH: =~ :/usr/lib64/openmpi/bin: ) ]]; then
+    export PATH=/usr/lib64/openmpi/bin:"$PATH"
+fi
 pip install numpy
 pip install matplotlib
