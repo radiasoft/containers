8303cf39806b75177be7611f1a9925db8c181da0
robnagler <github@q33.us>
2015-10-30 00:01:38 +0000

    allow alternative startup type

--- radiasoft/sirepo/sirepo-in-docker.sh
+++ radiasoft/sirepo/sirepo-in-docker.sh
@@ -2,8 +2,9 @@
 set -e
 run_dir=$1
 port=$2
+type=${3:-uwsgi --docker}
 . ~/.bashrc
 export PYTHONUNBUFFERED=1
 mkdir -p "$run_dir"
 cd "$run_dir"
-sirepo service uwsgi --run-dir "$run_dir" --port "$port" --docker >& start.log
+sirepo service $type --run-dir "$run_dir" --port "$port" >& start.log
