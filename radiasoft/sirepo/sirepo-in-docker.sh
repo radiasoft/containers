32d5135ee2753590a1faa2a0414826878cc3f178
robnagler <github@q33.us>
2015-10-30 00:18:31 +0000

    changed order of args so cleaner

--- radiasoft/sirepo/sirepo-in-docker.sh
+++ radiasoft/sirepo/sirepo-in-docker.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 set -e
+proto='uwsgi --docker'
+if [[ $1 == http ]]; then
+    proto=$1
+    shift
+fi
 run_dir=$1
 port=$2
-type=${3:-uwsgi --docker}
+if [[ ! -d $run_dir ]]; then
+    echo "$run_dir: run_dir doesn't exist" 1>&2
+    exit 1
+fi
+if [[ ! $port =~ ^[0-9]{2,5}$ ]]; then
+    echo "$port: port invalid port (100-99999)" 1>&2
+    exit 1
+fi
 . ~/.bashrc
 export PYTHONUNBUFFERED=1
 mkdir -p "$run_dir"
 cd "$run_dir"
-sirepo service $type --run-dir "$run_dir" --port "$port" >& start.log
+sirepo service $proto --run-dir "$run_dir" --port "$port" >& start.log
