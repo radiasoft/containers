8a60a6e029f66560af8b038d156f2afa80b1202f
robnagler <github@q33.us>
2015-11-12 22:38:26 +0000

    docker commands (in comments) for celery and rabbit

--- radiasoft/sirepo/sirepo-in-docker.sh
+++ radiasoft/sirepo/sirepo-in-docker.sh
@@ -20,3 +20,10 @@ export PYTHONUNBUFFERED=1
 mkdir -p "$db_dir"
 cd "$db_dir"
 sirepo service $proto --db-dir "$db_dir" --port "$port" >& start.log
+
+
+# docker run -i -t -v $PWD/run:/vagrant -p 8000:8000 --link rabbit:rabbit -u vagrant radiasoft/sirepo:20151111.232429 /bin/bash -i -c 'SIREPO_SERVER_JOB_QUEUE=Celery SIREPO_CELERY_TASKS_BROKER_URL=amqp://guest@rabbit// sirepo service http --port 8000 --db-dir /vagrant'
+
+# docker run --rm --link rabbit:rabbit --name celery -v $PWD/run:/vagrant -u vagrant radiasoft/sirepo:20151111.232429 /bin/bash -i -c 'SIREPO_CELERY_TASKS_BROKER_URL=amqp://guest@$RABBIT_PORT_5672_TCP_ADDR// celery worker -A sirepo.celery_tasks -l info'
+
+# docker run --rm --hostname rabbit --name rabbit -p 5672:5672 rabbitmq
