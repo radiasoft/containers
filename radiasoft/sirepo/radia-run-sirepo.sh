f08640dcb885c7408cdc257a28270ee9928b5d4d
robnagler <github@q33.us>
2015-11-17 19:06:17 -0700

    configure service with environment vars

--- radiasoft/sirepo/radia-run-sirepo.sh
+++ radiasoft/sirepo/radia-run-sirepo.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
-#
-# Start sirepo
-#
 set -e
-if [[ $RADIA_RUN_DIR && $RADIA_RUN_PORT ]]; then
-    db_dir=$RADIA_RUN_DIR
-    port=$RADIA_RUN_PORT
-else
-    db_dir=$1
-    port=$2
-fi
-if [[ $RADIA_RUN_RABBITMQ_HOST ]]; then
-    : ${RADIA_RUN_CHANNEL:=dev}
-    export SIREPO_SERVER_JOB_QUEUE=Celery
-    if [[ $RADIA_RUN_CHANNEL != dev ]]; then
-        SIREPO_SERVER_SESSION_SECRET=$(cat "$RADIA_RUN_DIR/sirepo_celery_secret" || exit 1)
-    fi
-    export SIREPO_SERVER_SESSION_KEY=sirepo_$RADIA_RUN_CHANNEL
-    export SIREPO_CELERY_TASKS_BROKER_URL=amqp://guest@"$RADIA_RUN_RABBITMQ_HOST"//
-fi
+db_dir=$1
+port=$2
 if [[ ! -d $db_dir ]]; then
     echo "$db_dir: db_dir doesn't exist" 1>&2
     exit 1
@@ -31,6 +14,7 @@ fi
 export PYTHONUNBUFFERED=1
 mkdir -p "$db_dir"
 cd "$db_dir"
-echo "$(date -u '+%Y-%m-%dT%H:%M:%SZ') sirepo service http --db-dir '$db_dir' --port '$port'" > start.log
-env >> start.log
-exec sirepo service http --db-dir "$db_dir" --port "$port" >> start.log 2>&1
+export SIREPO_PKCLI_SERVICE_PORT=$port
+export SIREPO_PKCLI_SERVICE_DB_DIR=$PWD
+(echo "$(date -u '+%Y-%m-%dT%H:%M:%SZ') sirepo service http; env) > start.log
+sirepo service http >> start.log 2>&1
