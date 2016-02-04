82295fe33b0ea6da1e00af798992c1f85c1f7447
git-user <git-email>
2016-02-04 20:54:43 +0000

    upgrade to latest SRW

--- radiasoft/beamsim/codes/srw.sh
+++ radiasoft/beamsim/codes/srw.sh
@@ -4,6 +4,24 @@ codes_yum install fftw2-devel
 codes_dependencies mpi4py
 #Too slow. git repo has too much junk:
 #   codes_download SRW
-codes_download https://depot.radiasoft.org/foss/SRW-20150923.tar.gz
+codes_download https://depot.radiasoft.org/foss/SRW-20160203.tar.gz
+perl -pi -e 's/-j8//' Makefile
+perl -pi -e "s/'fftw'/'sfftw'/" cpp/py/setup.py
+perl -pi -e 's/-lfftw/-lsfftw/; s/\bcc\b/gcc/; s/\bc\+\+/g++/' cpp/gcc/Makefile
 make
-make install
+d=$(python -c 'import distutils.sysconfig as s; print s.get_python_lib()')
+(
+    cd env/work/srw_python
+    install -m 644 {srwl,uti}*.py srwlpy.so "$d"
+)
+
+: Creating SRW*tar.gz <<'EOF'
+b=SRW-$(date +%Y%m%d)
+git clone https://github.com/ochubar/SRW
+cd SRW
+rm -rf .git literature env/{release,work/{install_proj,pre_releases,srw_igor}}
+find . -name '*.{old,a,so,pyd,lib,pdf}' -exec rm -f '{}' \;
+cd ..
+mv SRW "$b"
+tar czf "$b.tar.gz" "$b"
+EOF
