39b04c261d843127bc16b3af226246e97f96a6d6
robnagler <github@q33.us>
2016-03-02 18:08:31 +0000

    Fix #108: load new SRW & update SRW tarball instructions to make easier to build

--- radiasoft/beamsim/codes/srw.sh
+++ radiasoft/beamsim/codes/srw.sh
@@ -4,7 +4,7 @@ codes_yum install fftw2-devel
 codes_dependencies mpi4py
 #Too slow. git repo has too much junk:
 #   codes_download SRW
-codes_download https://depot.radiasoft.org/foss/SRW-20160203.tar.gz
+codes_download https://depot.radiasoft.org/foss/SRW-20160302.tar.gz
 perl -pi -e 's/-j8//' Makefile
 perl -pi -e "s/'fftw'/'sfftw'/" cpp/py/setup.py
 perl -pi -e 's/-lfftw/-lsfftw/; s/\bcc\b/gcc/; s/\bc\+\+/g++/' cpp/gcc/Makefile
@@ -16,12 +16,12 @@ d=$(python -c 'import distutils.sysconfig as s; print s.get_python_lib()')
 )
 
 : Creating SRW*tar.gz <<'EOF'
-b=SRW-$(date +%Y%m%d)
 git clone https://github.com/ochubar/SRW
 cd SRW
-rm -rf .git literature env/{release,work/{install_proj,pre_releases,srw_igor}}
+rm -rf .git literature env/{release,work/{install_proj,pre_releases,srw_igor,srw_python/data_example_*}}
 find . -name '*.{old,a,so,pyd,lib,pdf}' -exec rm -f '{}' \;
 cd ..
+b=SRW-$(date +%Y%m%d)
 mv SRW "$b"
 tar czf "$b.tar.gz" "$b"
 EOF
