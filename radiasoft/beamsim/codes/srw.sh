59358e01059a6e7f4c2a11bab7d16bac8826c3c9
robnagler <github@q33.us>
2016-03-04 22:04:29 +0000

    update to mrakitin/SRW@ff0fecf3f67eec183ef246e237e7c09d3fc4d2cb fixes radiasoft/sirepo#104

--- radiasoft/beamsim/codes/srw.sh
+++ radiasoft/beamsim/codes/srw.sh
@@ -4,7 +4,7 @@ codes_yum install fftw2-devel
 codes_dependencies mpi4py
 #Too slow. git repo has too much junk:
 #   codes_download SRW
-codes_download https://depot.radiasoft.org/foss/SRW-20160302.tar.gz
+codes_download https://depot.radiasoft.org/foss/SRW-20160304.tar.gz
 perl -pi -e 's/-j8//' Makefile
 perl -pi -e "s/'fftw'/'sfftw'/" cpp/py/setup.py
 perl -pi -e 's/-lfftw/-lsfftw/; s/\bcc\b/gcc/; s/\bc\+\+/g++/' cpp/gcc/Makefile
@@ -16,12 +16,24 @@ d=$(python -c 'import distutils.sysconfig as s; print s.get_python_lib()')
 )
 
 : Creating SRW*tar.gz <<'EOF'
-git clone https://github.com/ochubar/SRW
+cd ~/src/mrakitin
+test -d SRW || git clone https://github.com/mrakitin/SRW
 cd SRW
-rm -rf .git literature env/{release,work/{install_proj,pre_releases,srw_igor,srw_python/{data_example_*,lib/*}}}
-find . -name '*.{old,a,so,pyd,lib,pdf}' -exec rm -f '{}' \;
+git checkout master
+git pull --all
 cd ..
 b=SRW-$(date +%Y%m%d)
-mv SRW "$b"
+rsync -a --exclude .git SRW/. "$b"
+cd "$b"
+rm -rf literature env/{release,work/{install_proj,pre_releases,srw_igor,srw_python/{data_example_*,lib/*}}}
+find . -name '*.{old,a,so,pyd,lib,pdf}' -exec rm -f '{}' \;
+cd ..
 tar czf "$b.tar.gz" "$b"
+rm -rf "$b"
+echo "x=$PWD/$b.tar.gz"
+# As root
+d=/var/www/virtualhost/depot/foss/$(basename "$x")
+mv "$x" "$d"
+chown root:root "$d"
+chmod 444 "$d"
 EOF
