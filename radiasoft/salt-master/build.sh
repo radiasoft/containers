98e57dc95780dd2c92e1700813381c2b1b71346b
robnagler <github@q33.us>
2016-03-27 20:57:54 +0000

    remove all the default config

--- radiasoft/salt-master/build.sh
+++ radiasoft/salt-master/build.sh
@@ -3,6 +3,5 @@ build_fedora_base_image 23
 
 build_as_root() {
     # We don't want any default config. Set by the runner
-    rm -rf /etc/salt
-    ln -s /vagrant/etc/salt /etc/salt
+    rm -rf /etc/salt /var/cache/salt /srv
 }
