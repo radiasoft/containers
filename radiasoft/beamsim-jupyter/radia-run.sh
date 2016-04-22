6c171227be09995be9d3f629dac6fe3c000a5917
robnagler <github@q33.us>
2016-04-22 19:54:54 +0000

    setup synergia properly; typo in LANGUAGE

--- radiasoft/beamsim-jupyter/radia-run.sh
+++ radiasoft/beamsim-jupyter/radia-run.sh
@@ -2,12 +2,17 @@
 . ~/.bashrc
 
 set -e
-d=$(dirname "$(dirname "$(python -c 'import sys; sys.stdout.write(sys.executable)')")")
+
+# "synergia" command sets up the environment this way so we have to, too
+# Not clear if it will conflict with Warp or not
+d=$(dirname "$(dirname "$(python -c 'import sys; sys.stdout.write(sys.executable)')")")/lib
 export SYNERGIA2DIR=$d
-export LD_LIBRARY_PATH=$d
+export LD_LIBRARY_PATH=$d:/usr/lib64/openmpi/lib
+export PYTHONPATH=$d
+
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-export LNGUAGE=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
 
 # 8888 is hardwired everywhere. It should be JPY_IP and JPY_PORT
 exec {jupyterhub_singleuser} \
