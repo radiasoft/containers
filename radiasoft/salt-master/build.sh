5fb8b88f112c50d0f255e2455d18710bcae908ab
robnagler <github@q33.us>
2016-04-29 16:38:34 +0000

    enable -M again, need -X to not start daemons

--- radiasoft/salt-master/build.sh
+++ radiasoft/salt-master/build.sh
@@ -2,7 +2,7 @@
 build_fedora_base_image 23
 
 build_as_root() {
-    build_curl https://bootstrap.saltstack.com | bash -s -- -P -N -d -Z -n git develop
+    build_curl https://bootstrap.saltstack.com | bash -s -- -P -M -X -N -d -Z -n git develop
     # We don't want any default config, which is set by the runner
     rm -rf /etc/salt /var/cache/salt /srv
 }
