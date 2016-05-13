4824d6a09fe0b5bc19ba67329ff7d7416a1a2cb8
robnagler <github@q33.us>
2016-05-13 15:57:04 +0000

    pull dockspawner from robnagler

--- radiasoft/jupyterhub/build.sh
+++ radiasoft/jupyterhub/build.sh
@@ -6,7 +6,7 @@ build_docker_cmd='[]'
 build_as_root() {
     pip install 'ipython[all]'
     pip install git+git://github.com/jupyterhub/oauthenticator.git
-    pip install git+git://github.com/jupyterhub/dockerspawner.git
+    pip install git+git://github.com/robnagler/dockerspawner.git
     echo '# Real cfg in conf/jupyterhub_config.py' > /srv/jupyterhub/jupyterhub_config.py
     # Convenient to have "vagrant" user for development
     build_create_run_user
