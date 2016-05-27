4cb439ed84a80f8fb6e0cefddd981efd703daf0c
robnagler <github@q33.us>
2016-05-27 23:44:34 +0000

    use jupyterhub/dockerspawner and need psycopg2 for PG

--- radiasoft/jupyterhub/build.sh
+++ radiasoft/jupyterhub/build.sh
@@ -4,9 +4,10 @@ build_simply=1
 build_docker_cmd='[]'
 
 build_as_root() {
+    pip install psycopg2
     pip install 'ipython[all]'
     pip install git+git://github.com/jupyterhub/oauthenticator.git
-    pip install git+git://github.com/robnagler/dockerspawner.git
+    pip install git+git://github.com/jupyterhub/dockerspawner.git
     echo '# Real cfg in conf/jupyterhub_config.py' > /srv/jupyterhub/jupyterhub_config.py
     # Convenient to have "vagrant" user for development
     build_create_run_user
