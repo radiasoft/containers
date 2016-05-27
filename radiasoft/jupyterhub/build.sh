2023b19d71892d6047a7c43ac829625fb3fd946f
robnagler <github@q33.us>
2016-05-27 23:59:07 +0000

    install python-psycopg2 deb for postgres access

--- radiasoft/jupyterhub/build.sh
+++ radiasoft/jupyterhub/build.sh
@@ -4,11 +4,13 @@ build_simply=1
 build_docker_cmd='[]'
 
 build_as_root() {
-    pip install psycopg2
+    apt-get -y install python-psycopg2
     pip install 'ipython[all]'
     pip install git+git://github.com/jupyterhub/oauthenticator.git
     pip install git+git://github.com/jupyterhub/dockerspawner.git
     echo '# Real cfg in conf/jupyterhub_config.py' > /srv/jupyterhub/jupyterhub_config.py
     # Convenient to have "vagrant" user for development
     build_create_run_user
+    apt-get clean
+    rm -rf /var/lib/apt/lists/*
 }
