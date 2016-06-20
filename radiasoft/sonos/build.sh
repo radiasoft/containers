8d355c90955bdf7d291a76057f433660eddca75f
robnagler <github@q33.us>
2016-06-20 16:16:03 +0000

    fpc

--- radiasoft/sonos/build.sh
+++ radiasoft/sonos/build.sh
@@ -4,7 +4,7 @@ build_fedora_base_image 21
 build_as_root() {
     umask 022
     curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
-    build_yum nodejs
+    build_yum install nodejs
     cd /srv
     git clone https://github.com/jishi/node-sonos-http-api.git
     chown -R "$build_run_user:$build_run_user" node-sonos-http-api
