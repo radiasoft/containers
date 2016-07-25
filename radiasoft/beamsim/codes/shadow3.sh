39ec5e6a159eca38eccec4224f07892fcf4487ae
robnagler <github@q33.us>
2016-07-25 22:24:19 +0000

    shadow3 requirements.txt needs to be empty to avoid version conflict with numpy==1.9.3

--- radiasoft/beamsim/codes/shadow3.sh
+++ radiasoft/beamsim/codes/shadow3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 codes_dependencies pykern common
 codes_download robnagler/shadow3
-pip install -r requirements.txt
+# requirements already satisfied, and there is a version conflict with
+# the pip install -U in requirements.txt.
+echo '#' > requirements.txt
 python setup.py install
