8edf59b2a5e601e345416907eb1bd01d1e1d435a
robnagler <github@q33.us>
2016-07-28 21:38:04 +0000

    ensure bnlcrl is installed without upgrading numpy

--- radiasoft/beamsim/codes/srw.sh
+++ radiasoft/beamsim/codes/srw.sh
@@ -4,6 +4,9 @@ codes_yum install fftw2-devel
 codes_dependencies mpi4py
 # ochubar/SRW is over 600MB so GitHub times out sometimes. This is a
 # stripped down copy
+codes_download mrakitin/bnlcrl
+cat /dev/null > requirements.txt
+pip install
 codes_download SRW-light
 perl -pi -e 's/-j8//' Makefile
 perl -pi -e "s/'fftw'/'sfftw'/" cpp/py/setup.py
