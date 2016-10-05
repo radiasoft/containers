82f95f86e1b126d79cdd395b90eb3054985168ef
robnagler <github@q33.us>
2016-10-05 13:46:54 +0200

    make radia-run restartable with pkill

--- radiasoft/sirepo/radia-run-sirepo.sh
+++ radiasoft/sirepo/radia-run-sirepo.sh
@@ -12,9 +12,18 @@ if [[ ! $port =~ ^[0-9]{2,5}$ ]]; then
 fi
 . ~/.bashrc
 export PYTHONUNBUFFERED=1
-mkdir -p "$db_dir"
 cd "$db_dir"
+pid_file=$PWD/sirepo.pid
+if [[ -f $pid_file ]]; then
+    pkill -KILL -P "$(cat "$pid_file")" >& /dev/null || true
+    sleep 1
+    rm -f "$pid_file"
+fi
 export SIREPO_PKCLI_SERVICE_PORT=$port
 export SIREPO_PKCLI_SERVICE_DB_DIR=$PWD
 (echo "$(date -u '+%Y-%m-%dT%H:%M:%SZ') sirepo service http"; env) > start.log
-sirepo service http >> start.log 2>&1
+sirepo service http >> start.log 2>&1 &
+pid=$!
+echo "$pid" > "$pid_file"
+trap "pkill -KILL -P '$pid' >& /dev/null || true; rm -f '$pid_file'; exit" EXIT ERR INT TERM
+wait
