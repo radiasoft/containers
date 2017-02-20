2ec891283bd6cbea3aca51a5e54e3f6f59333562
robnagler <github@q33.us>
2017-02-20 20:39:08 +0000

    moved to container-beamsim-jupyter

--- radiasoft/beamsim-jupyter/jupyter_notebook_config.py
+++
@@ -1,5 +0,0 @@
-c.KernelSpecManager.whitelist = {'synergia-pre3', 'py2'}
-
-c.NotebookApp.terminado_settings = {
-    'shell_command': ['/bin/bash', '-l', '-i'],
-}
--- radiasoft/beamsim-jupyter/terminado-bash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-unset PYENV_VERSION PYENV_VIRTUAL_ENV
-exec /bin/bash -l -i
