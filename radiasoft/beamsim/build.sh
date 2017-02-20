53f4576c5199e212a8c6ed7c21d36535d607ca7e
robnagler <github@q33.us>
2017-02-20 03:14:28 +0000

    moved to container-* repos

--- radiasoft/beamsim/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-build_image_base=radiasoft/python2
-
-build_as_run_user() {
-    cd "$build_guest_conf"
-    . ./codes.sh
-}
--- radiasoft/testimage/radia-run-testimage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. ~/.bashrc
-set -e
-env
-python -c 'import json; assert float(json.load(open("/rsmanifest.json"))["version"]) > 20170101.'
-cat /rsmanifest.json
